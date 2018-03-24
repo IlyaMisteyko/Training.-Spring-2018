95f64cd1fb05293848881dc95db2fda89cae4842
Ilya Misteyko <ilya.misteyko@gmail.com>
2018-03-24 18:42:45 +0300

    Tasks

---
+++ NET.S.2018.Misteyko.04.05/Converter.Tests/StringExtentionTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using Converter;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter.Tests
+{
+    [TestFixture()]
+    public class StringExtentionTests
+    {
+        [TestCase("0110111101100001100001010111111", 2, 934331071)]
+        [TestCase("01101111011001100001010111111", 2, 233620159)]
+        [TestCase("11101101111011001100001010", 2, 62370570)]
+
+        //[TestCase("11111111111111111111111111111111" для основания 2->OverflowException)]
+        [TestCase("1AeF101", 16, 28242177)]
+        [TestCase("1ACB67", 16, 1756007)]
+        [TestCase("764241", 8, 256161)]
+        [TestCase("10", 5, 5)]
+        public void ToDecimalConverterTest(string source, int basis, int expected)
+        {
+            Notation n = new Notation(basis);
+            int actual = source.ToDecimalConverter(n);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[TestCase("1AeF101", 2)]
+        //[TestCase("SA123", 2)]
+        [TestCase("764241", 20)]
+        public void TestsOfArgumentException(string source, int basis)
+        {
+            Notation n = new Notation(basis);
+            Assert.Throws<System.ArgumentException>(() => source.ToDecimalConverter(n));
+        }
+    }
+}
---
+++ NET.S.2018.Misteyko.04.05/Converter/StringExtention.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    /// <summary>
+    /// Class which converts string to decimal number.
+    /// </summary>
+    public static class StringExtention
+    {
+        /// <summary>
+        /// Converts different number systems to decimal.
+        /// </summary>
+        /// <param name="source">Main string.</param>
+        /// <param name="notation">Object of class "Notation" which describe number system.</param>
+        /// <returns>Result of converting.</returns>
+        public static int ToDecimalConverter(this string source, Notation notation)
+        {
+            string sourceToUpper = source.ToUpper();
+            int result = 0;
+
+            for (int i = sourceToUpper.Length - 1, j = 1; i >= 0; i--, j *= notation.Basis)
+            {
+                result += notation.DesiredHalfOfTheAlphabet.IndexOf(sourceToUpper[i]) * j;
+            }
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Class which describe number system.
+    /// </summary>
+    public class Notation
+    {
+        /// <summary>
+        /// Field of the alphabet
+        /// </summary>
+        private string alphabet = "0123456789ABCDEF";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Notation"/> class.
+        /// </summary>
+        /// <param name="basis">The basis.</param>
+        /// <exception cref="System.ArgumentException">Wrong basis!</exception>
+        public Notation(int basis)
+        {
+            if (basis < 2 || basis > 16)
+            {
+                throw new ArgumentException("Wrong basis!");
+            }
+
+            this.Basis = basis;
+            this.DesiredHalfOfTheAlphabet = this.alphabet.Substring(0, this.Basis);
+        }
+
+        /// <summary>
+        /// Gets basis.
+        /// </summary>
+        /// <value>
+        /// Gets basis.
+        /// </value>
+        public int Basis { get; }
+
+        /// <summary>
+        /// Gets desired half of the alphabe.
+        /// </summary>
+        /// <value>
+        /// Gets desired half of the alphabet.
+        /// </value>
+        public string DesiredHalfOfTheAlphabet { get; }
+    }
+}
