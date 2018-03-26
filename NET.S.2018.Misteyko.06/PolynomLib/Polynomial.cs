f6300a362606d01bd0c3b1231d7d058797a7141f
Ilya Misteyko <ilya.misteyko@gmail.com>
2018-03-27 01:35:26 +0300

    Tasks

--- NET.S.2018.Misteyko.06/PolynomLib/Polynomial.cs
+++ NET.S.2018.Misteyko.06/PolynomLib/Polynomial.cs
@@ -37,6 +37,8 @@ namespace PolynomLib
             get { return polynom; }
         }
 
+        #region overloaded operations
+
         /// <summary>
         /// Overloaded operator "+".
         /// </summary>
@@ -183,7 +185,7 @@ namespace PolynomLib
         }
 
         /// <summary>
-        /// Overloaded operator "+".
+        /// Overloaded operator "-".
         /// </summary>
         /// <param name="num">Double number.</param>
         /// <param name="first">First polynom.</param>
@@ -231,7 +233,7 @@ namespace PolynomLib
             {
                 for (int i = 0; i < first.polynom.Length; i++)
                 {
-                    if (Math.Abs(first.polynom[i] - last.polynom[i]) > eps)
+                    if (Math.Abs(first.polynom[i]) - Math.Abs(last.polynom[i]) > eps)
                     {
                         return false;
                     }
@@ -257,6 +259,235 @@ namespace PolynomLib
             return true;
         }
 
+        #endregion
+
+        #region additional overloaded operations 
+
+        /// <summary>
+        /// Overloaded operation add.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="last">Second polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Add(Polynomial first, Polynomial last)
+        {
+            bool firstMax = first.polynom.Length > last.polynom.Length;
+            int maxLength = 0;
+            int minLength = 0;
+
+            if (firstMax == true)
+            {
+                maxLength = first.polynom.Length;
+                minLength = last.polynom.Length;
+            }
+            else
+            {
+                maxLength = last.polynom.Length;
+                minLength = first.polynom.Length;
+            }
+
+            double[] resultPolynom = new double[maxLength];
+
+            if (firstMax == true)
+            {
+                first.polynom.CopyTo(resultPolynom, 0);
+            }
+            else
+            {
+                last.polynom.CopyTo(resultPolynom, 0);
+            }
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (firstMax == true)
+                {
+                    resultPolynom[i] += last.polynom[i];
+                }
+                else
+                {
+                    resultPolynom[i] += first.polynom[i];
+                }
+            }
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation add.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="num">Double number.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Add(Polynomial first, double num)
+        {
+            double[] resultPolynom = new double[first.polynom.Length];
+            first.polynom.CopyTo(resultPolynom, 0);
+
+            resultPolynom[0] += num;
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation add.
+        /// </summary>
+        /// <param name="num">Double number.</param>
+        /// <param name="first">First polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Add(double num, Polynomial first)
+        {
+            double[] resultPolynom = new double[first.polynom.Length];
+            first.polynom.CopyTo(resultPolynom, 0);
+
+            resultPolynom[0] += num;
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation substruct.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="last">Second polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Substruct(Polynomial first, Polynomial last)
+        {
+            bool firstMax = first.polynom.Length > last.polynom.Length;
+            int maxLength = 0;
+            int minLength = 0;
+
+            if (firstMax == true)
+            {
+                maxLength = first.polynom.Length;
+                minLength = last.polynom.Length;
+            }
+            else
+            {
+                maxLength = last.polynom.Length;
+                minLength = first.polynom.Length;
+            }
+
+            double[] resultPolynom = new double[maxLength];
+
+            if (firstMax == true)
+            {
+                first.polynom.CopyTo(resultPolynom, 0);
+            }
+            else
+            {
+                last.polynom.CopyTo(resultPolynom, 0);
+            }
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (firstMax == true)
+                {
+                    resultPolynom[i] -= last.polynom[i];
+                }
+                else
+                {
+                    resultPolynom[i] -= first.polynom[i];
+                }
+            }
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation substruct.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="num">Double number.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Substruct(Polynomial first, double num)
+        {
+            double[] resultPolynom = new double[first.polynom.Length];
+            first.polynom.CopyTo(resultPolynom, 0);
+
+            resultPolynom[0] -= num;
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation substruct.
+        /// </summary>
+        /// <param name="num">Double number.</param>
+        /// <param name="first">First polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Substruct(double num, Polynomial first)
+        {
+            double[] resultPolynom = new double[first.polynom.Length];
+            first.polynom.CopyTo(resultPolynom, 0);
+
+            resultPolynom[0] -= num;
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation multiply.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="last">Second polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static Polynomial Multiply(Polynomial first, Polynomial last)
+        {
+            double[] resultPolynom = new double[(first.polynom.Length + last.polynom.Length) - 1];
+
+            for (int i = 0; i < first.polynom.Length; i++)
+            {
+                for (int j = 0; j < last.polynom.Length; j++)
+                {
+                    resultPolynom[i + j] += (first.polynom[i] * last.polynom[j]);
+                }
+            }
+
+            return new Polynomial(resultPolynom);
+        }
+
+        /// <summary>
+        /// Overloaded operation equals.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="last">Second polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static bool Equals(Polynomial first, Polynomial last)
+        {
+            if (first.polynom.Length == last.polynom.Length)
+            {
+                for (int i = 0; i < first.polynom.Length; i++)
+                {
+                    if (Math.Abs(first.polynom[i]) - Math.Abs(last.polynom[i]) > eps)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Overloaded operation compare.
+        /// </summary>
+        /// <param name="first">First polynom.</param>
+        /// <param name="last">Second polynom.</param>
+        /// <returns>Resulting polynom.</returns>
+        public static bool Compare(Polynomial first, Polynomial last)
+        {
+            if (first == last)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        #region override methods
+
         /// <summary>
         /// Override method "Equals".
         /// </summary>
@@ -278,7 +509,7 @@ namespace PolynomLib
 
             for (int i = 0; i < polynom.polynom.Length; i++)
             {
-                if (Math.Abs(polynom.polynom[i] - polynom.polynom[i]) > eps)
+                if (Math.Abs(polynom.polynom[i]) - Math.Abs(polynom.polynom[i]) > eps)
                 {
                     return false;
                 }
@@ -303,9 +534,38 @@ namespace PolynomLib
             return hash;
         }
 
-        //public override string ToString()
-        //{
-        //    return base.ToString();
-        //}
+        /// <summary>
+        /// Override method "ToString".
+        /// </summary>
+        /// <returns>Polynom in string view.</returns>
+        public override string ToString()
+        {
+            string resultStr = "";
+
+            if (polynom[0] != 0)
+            {
+                resultStr += $"{polynom[0]}";
+            }
+
+            for (int i = 1; i < polynom.Length; i++)
+            {
+                if (polynom[i] == 0)
+                {
+                    continue;
+                }
+
+                if (resultStr.Length == 0)
+                {
+                    resultStr += $"{polynom[i]}*x^{i}";
+                }
+                else
+                {
+                    resultStr += $"+{polynom[i]}*x^{i}";
+                }
+            }
+
+            return resultStr;
+        }
+        #endregion
     }
 }
