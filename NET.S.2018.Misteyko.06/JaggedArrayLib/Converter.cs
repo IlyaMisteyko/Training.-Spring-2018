f89616717ff8bcf8ee3b9eada0610030818a8e90
Ilya Misteyko <ilya.misteyko@gmail.com>
2018-04-08 18:48:56 +0300

    Tasks

--- NET.S.2018.Misteyko.06/JaggedArrayLib/Converter.cs
+++ NET.S.2018.Misteyko.06/JaggedArrayLib/Converter.cs
@@ -2,22 +2,46 @@
 
 namespace JaggedArrayLib
 {
+    /// <summary>
+    /// Converts delegate to interface
+    /// </summary>
     public static class Converter
     {
+        /// <summary>
+        /// Creates new delegate.
+        /// </summary>
+        /// <param name="del"></param>
+        /// <returns>New delegate.</returns>
         public static DelegateToInterface NewDelegate(Delegate del)
         {
             return new DelegateToInterface(del);
         }
 
+        /// <summary>
+        /// Implements the interface IComparer with delegate.
+        /// </summary>
         public class DelegateToInterface: IComparer
         {
+            /// <summary>
+            /// Field for delegate.
+            /// </summary>
             private Delegate del;
 
+            /// <summary>
+            /// Constructor for initialization.
+            /// </summary>
+            /// <param name="del"></param>
             public DelegateToInterface(Delegate del)
             {
                 this.del = del;
             }
 
+            /// <summary>
+            /// Method Compare
+            /// </summary>
+            /// <param name="first">First array.</param>
+            /// <param name="second">Second array.</param>
+            /// <returns>Result of comparing.</returns>
             public int Compare(int[] first, int[] second)
             {
                 return del.Invoke(first, second);
